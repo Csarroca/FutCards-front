c11e91504bacbb6ff6076adf3cc5e6cd66afeaaa
CSarroca <sarroca93@gmail.com>
2022-08-31 19:14:58 +0200

    Refactored register test for test handleSubmit

--- src/components/RegisterForm/RegisterForm.test.tsx
+++ src/components/RegisterForm/RegisterForm.test.tsx
@@ -2,6 +2,12 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import RegisterForm from "./RegisterForm";
 
+const mockUser = jest.fn();
+
+jest.mock("../../hooks/useUser", () => () => ({
+  register: mockUser,
+}));
+
 describe("Given a Register component", () => {
   describe("When its instantiaded", () => {
     test("Then it show a unsername and password inputs and a SING UP button", () => {
@@ -35,6 +41,28 @@ describe("Given a Register component", () => {
 
       expect(usernameInput.value).toBe(filledText1);
       expect(passwordInput.value).toBe(filledText2);
+    });
+  });
+  describe("When all inputs are fullfiled and the user clicks on the submit button", () => {
+    test("Then the register function will be called", async () => {
+      const filledText1 = "usertext";
+      const filledText2 = "userPasswd";
+      render(<RegisterForm />);
+
+      const usernameInput = screen.getByLabelText(
+        "Username"
+      ) as HTMLInputElement;
+      const passwordInput = screen.getByLabelText(
+        "Password"
+      ) as HTMLInputElement;
+
+      const submitButton = screen.getByRole("button", { name: "SING UP" });
+
+      await userEvent.type(usernameInput, filledText1);
+      await userEvent.type(passwordInput, filledText2);
+      await userEvent.click(submitButton);
+
+      expect(mockUser).toHaveBeenCalled();
     });
   });
 });
