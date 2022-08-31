2f676b199576f8b9fd6355613ed4a5423b57ee0a
CSarroca <sarroca93@gmail.com>
2022-08-31 15:51:54 +0200

    Refactored style of header

--- src/components/Header/HeaderStyled.tsx
+++ src/components/Header/HeaderStyled.tsx
@@ -1,14 +1,19 @@
 import styled from "styled-components";
 
 const HeaderStyled = styled.header`
-  .header-hidden {
-    clip: rect(0 0 0 0);
-    clip-path: inset(50%);
-    height: 1px;
-    overflow: hidden;
-    position: absolute;
-    white-space: nowrap;
-    width: 1px;
+  .header {
+    max-width: 350px;
+    display: flex;
+
+    &-hidden {
+      clip: rect(0 0 0 0);
+      clip-path: inset(50%);
+      height: 1px;
+      overflow: hidden;
+      position: absolute;
+      white-space: nowrap;
+      width: 1px;
+    }
   }
 `;
 
--- src/components/RegisterForm/RegisterFormStyled.tsx
+++ src/components/RegisterForm/RegisterFormStyled.tsx
@@ -3,10 +3,11 @@ import styles from "../../styles/styles";
 
 const RegisterFormStyled = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
+  align-items: flex-start;
   flex-direction: column;
   min-height: 100vh;
+  padding-left: 30px;
+  padding-right: 50px;
 
   .register {
     display: flex;
@@ -14,11 +15,24 @@ const RegisterFormStyled = styled.div`
     gap: 20px;
     &-title {
       color: ${styles.colors.globalDarkGreen};
+      font-weight: 600;
+      font-size: 24px;
     }
     &__input {
       background-color: ${styles.colors.backgroundLight};
       border: none;
+      border-bottom: 1px solid ${styles.colors.darkButtonHover};
     }
+  }
+  label {
+    font-weight: 400;
+    font-size: 14px;
+    color: ${styles.colors.darkButtonHover};
+  }
+  input {
+    width: 100%;
+    padding-left: 15px;
+    padding-bottom: 15px;
   }
 `;
 
