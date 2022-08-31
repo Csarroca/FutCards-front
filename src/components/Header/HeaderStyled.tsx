9da3f92bbf669d279e252d8941b21dd240a84957
CSarroca <sarroca93@gmail.com>
2022-08-31 16:42:28 +0200

    Refactored test for header for it visibility

--- src/components/Header/HeaderStyled.tsx
+++ src/components/Header/HeaderStyled.tsx
@@ -6,13 +6,7 @@ const HeaderStyled = styled.header`
     display: flex;
 
     &-hidden {
-      clip: rect(0 0 0 0);
-      clip-path: inset(50%);
-      height: 1px;
-      overflow: hidden;
-      position: absolute;
-      white-space: nowrap;
-      width: 1px;
+      display: none;
     }
   }
 `;
