0b41ed330daa6540c6359505d023f4cd34b18f8b
CSarroca <sarroca93@gmail.com>
2022-09-02 16:25:59 +0200

    Added toastify modals at register component and hook

--- src/mooks/handlers.ts
+++ src/mooks/handlers.ts
@@ -25,4 +25,28 @@ export const handlers = [
       );
     }
   ),
+  rest.post(
+    `${process.env.REACT_APP_API_URL}users/login`,
+    async (req, res, ctx) => {
+      const body = await req.json();
+      if (!body.userName || !body.password) {
+        return res(
+          ctx.status(400),
+          ctx.json({
+            error: "Wrong data",
+          })
+        );
+      }
+
+      return res(
+        ctx.status(201),
+        ctx.json([
+          {
+            userName: "paco",
+            password: "paco12345",
+          },
+        ])
+      );
+    }
+  ),
 ];
