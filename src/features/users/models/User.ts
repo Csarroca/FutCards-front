5759bbdea543e53073ce2dabc4b3466722e39e1d
CSarroca <sarroca93@gmail.com>
2022-09-02 13:14:21 +0200

    Created login function at useUser hook

---
+++ src/features/users/models/User.ts
@@ -0,0 +1,10 @@
+export interface ProtoUser {
+  userName: string;
+  password: string;
+}
+
+export interface User {
+  id: string;
+  userName: string;
+  token: string;
+}
--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,4 +1,4 @@
-export interface UserRegister {
+export interface AuthData {
   userName: string;
   password: string;
 }
