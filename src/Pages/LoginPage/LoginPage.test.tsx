0a5da767ebbea74b8898fb06db7ce9adf3c74b98
CSarroca <sarroca93@gmail.com>
2022-09-02 20:29:23 +0200

    Created LoginPage and tested the component

--- src/Pages/LoginPage/LoginPage.test.tsx
+++ src/Pages/LoginPage/LoginPage.test.tsx
@@ -1,23 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Wrapper from "../../utils/Wrapper";
-import RegisterPage from "./RegisterPage";
+import RegisterPage from "./LoginPage";
 
 describe("Given a RegisterPage component", () => {
   describe("When it is instanciated", () => {
     test("Then it should show a heading with 'Create an account' as a text", () => {
-      const titleText = "Create an account";
+      const titleText = "Sign in";
 
       render(<RegisterPage />, { wrapper: Wrapper });
 
       const headingName = screen.getByRole("heading", { name: titleText });
       expect(headingName).toBeInTheDocument();
-    });
-    test("Then it should show a Register component form", () => {
-      render(<RegisterPage />, { wrapper: Wrapper });
-
-      const form = screen.getByLabelText("Username");
-
-      expect(form).toBeInTheDocument();
     });
   });
 });
