70351113a0ab5e64ecc0f108412511065330c179
CSarroca <sarroca93@gmail.com>
2022-09-03 16:13:36 +0200

    Created Login component

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,25 @@
+import Button from "../../components/Button/Button";
+import { Link } from "react-router-dom";
+
 const LoginPage = (): JSX.Element => {
-  return <h2 className="Login-title">Sign in</h2>;
+  return (
+    <>
+      <h2 className="Login-title">Sign in</h2>
+
+      <span className="register-text__text">
+        Don't have an account?
+        <Link to={"/register"}>
+          <Button
+            actionOnclick={() => {}}
+            buttonText={"Create an account"}
+            className=""
+            isDisabled={false}
+            type="button"
+          />
+        </Link>
+      </span>
+    </>
+  );
 };
 
 export default LoginPage;
---
+++ src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,76 @@
+import React, { SyntheticEvent, useState } from "react";
+import useUser from "../../hooks/useUser";
+import Button from "../Button/Button";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const LoginForm = (): JSX.Element => {
+  const { login } = useUser();
+
+  const initialState = {
+    userName: "",
+    password: "",
+  };
+
+  const [loginData, setLoginData] = useState(initialState);
+
+  const formValidate = loginData.userName !== "" && loginData.password !== "";
+
+  const handleSubmit = async (event: SyntheticEvent) => {
+    event.preventDefault();
+
+    await login(loginData);
+    setLoginData(initialState);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginData({
+      ...loginData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  return (
+    <>
+      <ToastContainer />
+      <form onSubmit={handleSubmit} className="register" noValidate>
+        <label htmlFor="userName">Username</label>
+
+        <input
+          className="Login__input"
+          id="userName"
+          type="text"
+          name="userName"
+          placeholder="Username"
+          required
+          autoComplete="off"
+          value={loginData.userName}
+          onChange={handleChange}
+        />
+        <label htmlFor="password">Password</label>
+
+        <input
+          className="Login__input"
+          id="password"
+          type="password"
+          name="password"
+          placeholder="password"
+          required
+          autoComplete="off"
+          value={loginData.password}
+          onChange={handleChange}
+        />
+
+        <Button
+          isDisabled={!formValidate}
+          type={"submit"}
+          className="Login__submit"
+          actionOnclick={() => {}}
+          buttonText={"Login"}
+        ></Button>
+      </form>
+    </>
+  );
+};
+
+export default LoginForm;
