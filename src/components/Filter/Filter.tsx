aee80b9e8f70fec6c3a5e54fd44ec5b6afa0be3e
CSarroca <sarroca93@gmail.com>
2022-09-12 18:27:19 +0200

    Created Filter component

---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,48 @@
+import { SyntheticEvent, useState } from "react";
+import useApi from "../../hooks/cards/useAPI";
+import FilterStyled from "./FilterStyled";
+
+const Filter = (): JSX.Element => {
+  const [state, setState] = useState({ position: "All" });
+  const { getByPosition, getAllCards } = useApi();
+
+  const onChangeSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setState({
+      ...state,
+      [event.target.id]: event.target.value,
+    });
+  };
+  const handleGetPosition = async (event: SyntheticEvent) => {
+    event.preventDefault();
+    if (state.position === "All") {
+      await getAllCards();
+      return;
+    }
+    await getByPosition(state.position);
+  };
+  return (
+    <FilterStyled noValidate onSubmit={handleGetPosition}>
+      <div className="input-container">
+        <label htmlFor="Position" className="title">
+          Position
+        </label>
+        <select
+          id="position"
+          onChange={onChangeSelect}
+          className="select-input"
+        >
+          <option>All</option>
+          <option>ST</option>
+          <option>MC</option>
+          <option>DFC</option>
+          <option>GK</option>
+        </select>
+      </div>
+      <button className="button" type="submit">
+        FILTER
+      </button>
+    </FilterStyled>
+  );
+};
+
+export default Filter;
