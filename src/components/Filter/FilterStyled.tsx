87b22dbc504d77ee88e4e6a1429ee6cf898b4553
CSarroca <sarroca93@gmail.com>
2022-09-13 10:25:50 +0200

    Added style to filter

--- src/components/Filter/FilterStyled.tsx
+++ src/components/Filter/FilterStyled.tsx
@@ -1,5 +1,33 @@
 import styled from "styled-components";
+import styles from "../../styles/styles";
 
-const FilterStyled = styled.form``;
+const FilterStyled = styled.form`
+  display: flex;
+  gap: 20px;
+  margin-bottom: 20px;
+  background-color: ${styles.colors.globalGreen};
+
+  width: 90%;
+  .title {
+    color: white;
+    padding: 10px;
+  }
+  .select-input {
+    background-color: ${styles.colors.globalGreen};
+    color: white;
+    border: none;
+  }
+
+  .button {
+    background-color: ${styles.colors.globalGreen};
+    color: white;
+    border: none;
+
+    :hover {
+      font-weight: bold;
+      background-color: ${styles.colors.globalDarkGreen};
+    }
+  }
+`;
 
 export default FilterStyled;
